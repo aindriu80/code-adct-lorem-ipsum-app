f33d1d57fef537faa67b7ab56b0822d77d9a09e7
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-20 11:53:42 +0100

    How the app should look like - statically generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
-import { useState } from 'react'
-import data from './data'
-import './App.css'
+import { useState } from 'react';
+import data from './data';
+import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Hello from the terminal');
+  };
+
   return (
-    <div className="App">
-      <h2>Lorem Ipsum App</h2>
-    </div>
-  )
+    <section className='section-center'>
+      <h3>Lorem Ipsum App</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>Paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          Generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+          dignissimos ratione quod magni deleniti quidem, id in sunt a fugiat
+          ducimus reprehenderit error quisquam vitae nam rem qui, modi quam!
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+          dignissimos ratione quod magni deleniti quidem, id in sunt a fugiat
+          ducimus reprehenderit error quisquam vitae nam rem qui, modi quam!
+        </p>
+      </article>
+    </section>
+  );
 }
 
-export default App
+export default App;
