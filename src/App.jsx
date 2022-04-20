e44d171ea23c8c45a4a5761ec84c3e758125d9f8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-20 11:40:44 +0100

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import data from './data'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Lorem Ipsum App</h2>
+    </div>
+  )
+}
+
+export default App
